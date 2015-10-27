eecaec75d40d6b3f72f1d9ca605e8701e49b6e98
zhangyuhui <darlyyuhui@hotmail.com>
2015-10-27 17:46:33 +0800

    新建一个类

---
+++ Activities/src/com/darly/activities/ui/fragment/me/MeSecActivity.java
@@ -0,0 +1,69 @@
+/**
+ * 下午5:45:11
+ * @author Zhangyuhui
+ * $
+ * MeSecActivity.java
+ * TODO
+ */
+package com.darly.activities.ui.fragment.me;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.darly.activities.base.BaseActivity;
+
+/**
+ * @author Zhangyuhui
+ * MeSecActivity
+ * $
+ * 下午5:45:11
+ * TODO
+ */
+public class MeSecActivity extends BaseActivity {
+
+	/* (non-Javadoc)
+	 * @see android.view.View.OnClickListener#onClick(android.view.View)
+	 */
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.darly.activities.base.BaseActivity#initView(android.os.Bundle)
+	 */
+	@Override
+	public void initView(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.darly.activities.base.BaseActivity#initData()
+	 */
+	@Override
+	public void initData() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.darly.activities.base.BaseActivity#refreshGet(java.lang.Object)
+	 */
+	@Override
+	public void refreshGet(Object object) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.darly.activities.base.BaseActivity#refreshPost(java.lang.Object)
+	 */
+	@Override
+	public void refreshPost(Object object) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
